e3866a44e7af2fdeb1daf7c87c2e40707e813e8f
Pawan Gupta <pawan-crestron>
2022-10-13 00:00:45 +0530

    Configured Security Added Open Feign for calling other services

--- src/main/java/com/wittgroup/kyn/profile/KynProfileApplication.java
+++ src/main/java/com/wittgroup/kyn/profile/KynProfileApplication.java
@@ -1,12 +1,27 @@
 package com.wittgroup.kyn.profile;
 
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.protocol.HttpContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.web.servlet.oauth2.client.OAuth2ClientSecurityMarker;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.WebRequestInterceptor;
+
+import java.io.IOException;
 
 @SpringBootApplication
+@EnableFeignClients
 public class KynProfileApplication {
 
     public static void main(final String[] args) {
@@ -14,9 +29,10 @@ public class KynProfileApplication {
     }
 
     @Bean
-    @LoadBalanced
-    RestTemplate restTemplate() {
-        return new RestTemplate();
+    RequestInterceptor requestTokenBearerInterceptor() {
+        return requestTemplate -> {
+            JwtAuthenticationToken token = (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+            requestTemplate.header("Authorization", "Bearer " + token.getToken().getTokenValue());
+        };
     }
-
 }
