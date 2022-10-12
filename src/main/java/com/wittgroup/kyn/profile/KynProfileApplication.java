ca8194bccab5f2a6e5b8d613f8ec960e888ac781
Pawan Gupta <pawan-crestron>
2022-10-13 00:20:44 +0530

    Removed unused imports.

--- src/main/java/com/wittgroup/kyn/profile/KynProfileApplication.java
+++ src/main/java/com/wittgroup/kyn/profile/KynProfileApplication.java
@@ -1,24 +1,12 @@
 package com.wittgroup.kyn.profile;
 
 import feign.RequestInterceptor;
-import feign.RequestTemplate;
-import org.apache.http.HttpException;
-import org.apache.http.HttpRequest;
-import org.apache.http.HttpRequestInterceptor;
-import org.apache.http.protocol.HttpContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
-import org.springframework.security.config.web.servlet.oauth2.client.OAuth2ClientSecurityMarker;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.context.request.WebRequestInterceptor;
-
-import java.io.IOException;
 
 @SpringBootApplication
 @EnableFeignClients
