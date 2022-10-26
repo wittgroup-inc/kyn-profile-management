acfce4d714daf821cb54bc92bd13c3e28f1de159
Pawan Gupta <pawan-crestron>
2022-10-26 19:02:08 +0530

    Added  - Load User API  - Ignored security for Load and Create user.

--- src/main/java/com/wittgroup/kyn/profile/db/repositories/ProfileRepository.java
+++ src/main/java/com/wittgroup/kyn/profile/db/repositories/ProfileRepository.java
@@ -11,4 +11,5 @@ public interface ProfileRepository extends MongoRepository<ProfileEntity, UUID> 
     Optional<ProfileEntity> findBySettingsUsername(String user);
     Optional<ProfileEntity> findBySettingsPrimaryEmail(String email);
     Optional<ProfileEntity> findBySettingsRegisteredMobileNumber(String mobile);
+    Optional<ProfileEntity> findBySettingsUsernameOrSettingsPrimaryEmailOrSettingsRegisteredMobileNumber(String username, String email, String mobileNumber);
 }
