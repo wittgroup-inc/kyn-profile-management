a7b4a4d95377a166d7970907d059a3f4a87ea426
Pawan Gupta <pawankgupta.se@gmail.com>
2023-03-29 21:39:35 +0530

    Fix issue with mongodb.

--- src/main/java/com/gowittgroup/kyn/profile/config/MongoConfig.java
+++ src/main/java/com/gowittgroup/kyn/profile/config/MongoConfig.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 
 
 @Configuration
-@EnableMongoRepositories("com.wittgroup.kyn.profile.db.repositories")
+@EnableMongoRepositories("com.gowittgroup.kyn.profile.db.repositories")
 @EnableMongoAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
 public class MongoConfig {
 
